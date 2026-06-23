8908f2abcfdf3615f8bef05eff3252b3d9cb003b
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-06-22 21:21:50 -0500

    Import and configure Figtree Google Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import { Metadata } from "next";
+import { Figtree } from 'next/font/google';
 import "./globals.css";
+import { NextFont } from "next/dist/compiled/@next/font";
 
 export const metadata: Metadata = {
   title: "Blog Preview Card",
   description: "",
 };
+
+const figtree: NextFont = Figtree({ subsets: ['latin']});
 
 export default function RootLayout({
   children,
@@ -14,6 +18,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      className={figtree.className}
     >
       <body>{children}</body>
     </html>
