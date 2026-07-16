5d4a68d5ad14504cb294dcd21cba07f90b486705
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-07-16 08:47:01 -0500

    Create basic structure and styles for the card

--- app/components/BlogPreviewCard.tsx
+++ app/components/BlogPreviewCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from './BlogPreviewCard.module.css';
 
 type BlogPreviewCardProps = {
@@ -12,7 +13,20 @@ type BlogPreviewCardProps = {
 const BlogPreviewCard = ({ tag, date, title, text, name, avatar }: BlogPreviewCardProps) => {
   return (
     <div className={styles.container}>
-
+      <article className={styles.card}>
+        <section className={styles.imageContainer}>
+          <Image
+            src={"/images/illustration-article.svg"}
+            alt="decorative illustration with some coding symbols"
+            fill
+            className={styles.image}
+          />
+        </section>
+        <section className={styles.content}>
+        </section>
+        <figure className={styles.author}>
+        </figure>
+      </article>
     </div>
   );
 };
