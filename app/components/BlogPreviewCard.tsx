9a3e15ad07640c3015f0fe7b03a2d8bb5b1589e8
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-07-16 13:42:16 -0500

    Fix accessibility issues (wrong semantic elements, alts, etc.)

--- app/components/BlogPreviewCard.tsx
+++ app/components/BlogPreviewCard.tsx
@@ -24,34 +24,35 @@ const BlogPreviewCard = ({ tag, date, title, text, name, avatar }: BlogPreviewCa
   return (
     <div className={styles.container}>
       <article className={styles.card}>
-        <section className={styles.imageContainer}>
+        <div className={styles.imageContainer}>
           <Image
             src={"/images/illustration-article.svg"}
-            alt="decorative illustration with some coding symbols"
+            alt=""
             fill
             className={styles.image}
           />
-        </section>
-        <section className={styles.content}>
-          <button className={styles.articleTag}>{tag}</button>
+        </div>
+        <div className={styles.content}>
+          <span className={styles.articleTag}>{tag}</span>
           <time className={styles.articleDate} dateTime={date}>Published {dateFormatted}</time>
           <Link href="https://www.google.com/aclk?sa=L&pf=1&ai=DChsSEwjRw6e12dCVAxVccX8AHT-VJXEYACICCAEQABoCb2E&co=1&ase=2&gclid=CjwKCAjwmdLSBhANEiwAkREMN1a28RiN1fNVyS2wcGegvoXBgklZyf1vYH1mmX4IGGtZ-HJyYR46ExoC1xcQAvD_BwE&cid=CAAS9gHkaOCrSoiD_yotRGjuF-eD22qLRAQ05zxeOfJD0F4l_vlnDwX_9bFxyWxegIoha84QJjiNdfBh1Dftxvif342_xSJBvb_bjgHuXdvfS8BOP3e6tCqQq2hloAZmDQ6-VIbG7fcDzIaGLHH9ROguxRyd_qvxXdksOzJXAyd0VMYSXV6pu5jULG94Kmh5kimNARP0mJvpIuGaG5m0zrH3YL8jg1DdM2pV3XZi-ENKJWXuscoDORlar4Vu4bKAniG_OdO93WDiJYe1q6hiKivrmDj3ZZZMc-f_EXgRX23v5LHZ1krm11jO1NJTszsWdM1TaropF67Z12w&cce=2&category=acrcp_v1_32&sig=AOD64_2gqQZzgzy_HHth2zlL2x9-O-RHVA&q&nis=4&adurl=https://www.udemy.com/course/html-and-css-foundations/?utm_campaign%3DSearch_Keyword_Beta_Prof_la.EN_cc.US%26utm_source%3Dgoogle%26utm_medium%3Dpaid-search%26portfolio%3DUSA%26utm_audience%3Dmx%26utm_tactic%3Dnb%26utm_term%3D_._ag_165617438568_._ad_698471127305_._kw_learn%2520html%26utm_content%3Dg%26funnel%3D%26test%3D%26gad_source%3D1%26gad_campaignid%3D21099451329%26gbraid%3D0AAAAADROdO3lkaXjKtp3o77HKSJFrEBjF%26gclid%3DCjwKCAjwmdLSBhANEiwAkREMN1a28RiN1fNVyS2wcGegvoXBgklZyf1vYH1mmX4IGGtZ-HJyYR46ExoC1xcQAvD_BwE&ved=2ahUKEwifo6O12dCVAxX_68kDHUfCKSwQ0Qx6BAgZEAE"
-            className={styles.articleTitle}>
-              {title}
+            className={styles.articleTitle}
+          >
+            <h2>{title}</h2>
           </Link>         
           <p className={styles.articleDescription}>{text}</p>
-        </section>
-        <figure className={styles.author}>
+        </div>
+        <footer className={styles.author}>
           <div className={styles.avatarContainer}>
             <Image
               src={avatar}
-              alt="author's avatar"
+              alt={`${name}'s avatar`}
               fill
               className={styles.avatar}
             />
           </div>
-          <figcaption className={styles.authorName}>{name}</figcaption>
-        </figure>
+          <span className={styles.authorName}>{name}</span>
+        </footer>
       </article>
     </div>
   );
