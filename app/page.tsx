b507fd3b86e85454cbb1d832dd45cbe5141e5aa6
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-07-16 13:56:57 -0500

    Display the instance of the card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import BlogPreviewCard from "./components/BlogPreviewCard/BlogPreviewCard";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <main>
-
+        <BlogPreviewCard
+          tag="Learning"
+          date="2023-12-21"
+          title="HTML & CSS foundations"
+          description="These languages are the backbone of every website, defining structure, content, and presentation."
+          name="Greg Hooper"
+          avatar="/images/image-avatar.webp"
+        />
       </main>
     </div>
   );
